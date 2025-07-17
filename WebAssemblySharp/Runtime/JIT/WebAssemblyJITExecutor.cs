0297895265e9e636dc86c3386d21f943f7e19e49
Christian Schlegel <christian.schlegel@outlook.com>
2025-07-17 20:26:31 +0200

    Add `MethodImplOptions` support to WebAssembly JIT executor.

--- WebAssemblySharp/Runtime/JIT/WebAssemblyJITExecutor.cs
+++ WebAssemblySharp/Runtime/JIT/WebAssemblyJITExecutor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using WebAssemblySharp.MetaData;
 using WebAssemblySharp.MetaData.Utils;
